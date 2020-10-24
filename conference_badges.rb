a8b6ced0dd74c26801e95c26621f45ae9680d75f
Erin Anderson <e.anderson1226@gmail.com>
2020-10-24 09:51:07 -0400

    commit tree ce9de579ae459e11977d3bde19f3bb97f55de20a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    attendees = []
+    names.map do |name| 
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(people)
+    new_array = []
+    people.each_with_index do |name, assignment|
+    new_array << "Hello, #{name}! You'll be assigned to room #{assignment + 1}!"
+    end
+    new_array
+end
+
+def printer(identifiers)
+    batch_badge_creator(identifiers).each do |badges|
+   puts badges
+   end
+   assign_rooms(identifiers).each do |room_assignments|
+   puts room_assignments
+   end
+end
